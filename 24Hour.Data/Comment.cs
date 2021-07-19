4d3f3fd5db0a9e5a4e7e7a0207cb1270f388c84a
Alexandria Hardin <aphardin101@gmail.com>
2021-07-19 16:03:15 -0500

    issues with merge

---
+++ 24Hour.Data/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        //public virtual List<Reply> replies = new List<Reply>();
+
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
