c1c358b68d8dd1a486b3719eaf934f5d22ea01b1
Julie Bishop <juliebishopphotos@gmail.com>
2021-07-20 12:36:02 -0400

    fixing file structure

---
+++ 24Hour.Data/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        //public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
---
+++ 24Hour.Models/PostCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Models
+{
+    public class PostCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters")]
+        [MaxLength(100, ErrorMessage = "There are too man characters in this field")]
+        public string Title { get; set; }
+        [MaxLength(240, ErrorMessage = "240 character max")]
+        public string Text { get; set; }
+    }
+}
