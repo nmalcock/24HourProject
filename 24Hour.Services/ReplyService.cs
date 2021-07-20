f767e5b12f0e847cd3e26c9b6d48cea913e96437
nmalcock <nmalcock97@gmail.com>
2021-07-20 13:57:15 -0400

    Adding Reply Service

--- 24Hour.Services/ReplyService.cs
+++ 24Hour.Services/ReplyService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _24Hour.Data;
+using _24Hour.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace _24Hour.Services
 {
-    class ReplyService
+    public class ReplyService
     {
+        private readonly Guid _authorId;
+
+        public ReplyService(Guid authorId)
+        {
+            _authorId = authorId;
+        }
+
+        public bool CreateReply(ReplyCreate model)
+        {
+            var entity =
+                new Reply()
+                {
+                    Text = model.Text,
+                    CommentId = model.CommentId,
+                    AuthorId = _authorId
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
