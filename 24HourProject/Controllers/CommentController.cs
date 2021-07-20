c50d48e5ac7e4368a48cdd9be0d470e969ffdb50
Alexandria Hardin <aphardin101@gmail.com>
2021-07-20 11:17:54 -0500

    added comment and reply controller drafts

---
+++ 24HourProject/Controllers/CommentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24HourProject.Controllers
+{
+    [Authorize]
+    public class CommentController : ApiController
+    {
+        //public IHttpActionResult Get()
+        //{
+        //    CommentService commentService = CreateCommentService();
+        //    var comments = commentService.GetComment();
+        //    return Ok(comments);
+        //}
+
+        //public IHttpActionResult Get(int id)
+        //{
+        //    CommentService commentService = CreateCommentService();
+        //    var comment = commentService.GetCommentById(id);
+        //    return Ok(comment);
+        //}
+        //public IHttpActionResult Post(CommentCreate comment)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return BadRequest(ModelState);
+        //    var service = CreateCommentService();
+        //    if (!service.CreateComment(comment))
+        //        return InternalServerError();
+        //    return Ok();
+        //}
+        //private CommentService CreateCommentService()
+        //{
+        //    var authorId = Guid.Parse(User.Identity.GetAuthorId());
+        //    var commentService = new CommentService(authorId);
+        //    return commentService;
+        }
+    }
